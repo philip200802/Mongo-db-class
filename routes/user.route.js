7bfc6ec79525d228f341f15a07b04ff52b2f6fa2
philip2008 <adegboyegaphilip401@gmail.com>
2026-04-16 09:46:08 +0100

    Update

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
-// const { postSignup, getSignup, postSignin, getSignin, getDashboard } = require("../controllers/user.controllers");
-const { postSignup, getSignup, postSignin, getSignin, getDashboard } = require("../controllers/user.controller");
+const { postSignup, getSignup, postSignin, getSignin, getDashboard, getAllUsers } = require("../controllers/user.controller");
 router.get("/signup", getSignup);
 router.post("/register", postSignup);
 router.get("/signin", getSignin);
 router.post("/login", postSignin);
 router.get("/dashboard", getDashboard);
-
+router.get("/registeredUsers", getAllUsers)
 
 module.exports = router;
